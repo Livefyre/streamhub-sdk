380f6a55f246f30404590231ce342df5f6a2bc2c
Ryan Cheung <ryanc@livefyre.com>
2013-11-20 18:20:12 -0800

    Fix bugs exposed by broken unit tests

--- tests/spec/content/views/content-view.js
+++ tests/spec/content/views/content-view.js
@@ -39,7 +39,7 @@ function ($, jasmine, jasmineJquery, util, Content, LivefyreContent, ContentView
                     doc = document.documentElement;//sandbox(); ?
                 $obj.prependTo(doc);
                 expect($.contains(doc, elem)).toBe(true);
-                
+                debugger; 
                 contentView.remove();
                 
                 expect($.contains(doc, elem)).toBe(false);
