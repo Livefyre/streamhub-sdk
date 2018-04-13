76362d7c5a174b5262fa11b455298f43aaaaf7f3
Joseph Ross <joross@adobe.com>
2018-04-13 11:06:38 -0700

    Switched view to 4 space indentation, bumped version

--- src/content/views/call-to-action-bar-view.js
+++ src/content/views/call-to-action-bar-view.js
@@ -24,9 +24,9 @@ var EFFECTIVE_CTA_WIDTH = CTA_WIDTH + SPACE_BETWEEN_CTAS;
  * @constructor
  */
 var CallToActionBar = function (opts) {
-  opts = opts || {};
-  View.call(this, opts);
-  window.addEventListener('resize', this.render.bind(this));
+    opts = opts || {};
+    View.call(this, opts);
+    window.addEventListener('resize', this.render.bind(this));
 };
 inherits(CallToActionBar, View);
 
@@ -43,75 +43,74 @@ CallToActionBar.prototype.contentAttr = 'data-content-id';
 CallToActionBar.prototype.insightsVerb = 'CustomCtaButtonClick';
 
 CallToActionBar.prototype.events = View.prototype.events.extended({}, function (events) {
-  events['click ' + this.buttonSelector] = this.onButtonClick.bind(this);
-  events['keyup ' + this.buttonSelector] = this.onButtonClick.bind(this);
-  events['keyup a'] = this.onAnchorKeyUp.bind(this);
+    events['click ' + this.buttonSelector] = this.onButtonClick.bind(this);
+    events['keyup ' + this.buttonSelector] = this.onButtonClick.bind(this);
+    events['keyup a'] = this.onAnchorKeyUp.bind(this);
 });
 
 CallToActionBar.prototype.render = function () {
-  if (!this.opts.content.links.cta || this.opts.content.links.cta.length === 0) {
-    return;
-  }
+    if (!this.opts.content.links.cta || this.opts.content.links.cta.length === 0) {
+        return;
+    }
 
-  View.prototype.render.call(this);
+    View.prototype.render.call(this);
 };
 
 CallToActionBar.prototype.onButtonClick = function (e) {
-  if (!e.keyCode || e.keyCode === 13 || e.keyCode === 32) {
-    this.togglePopover();
-  }
+    if (!e.keyCode || e.keyCode === 13 || e.keyCode === 32) {
+        this.togglePopover();
+    }
 };
 
 CallToActionBar.prototype.onAnchorKeyUp = function (e) {
-  if (e.keyCode === 13 || e.keyCode === 32) {
-    this.$el.trigger('insights:local', { type: this.insightsVerb });
-  }
-  return false;
-}
+    if (e.keyCode === 13 || e.keyCode === 32) {
+        this.$el.trigger('insights:local', { type: this.insightsVerb });
+    }
+};
 
 CallToActionBar.prototype.togglePopover = function () {
-  this.$el.find(this.popoverSelector).toggleClass(this.showClass);
-  this.$el.find(this.buttonIconSelector).toggleClass([this.buttonOpenClass, this.buttonClosedClass].join(' '));
+    this.$el.find(this.popoverSelector).toggleClass(this.showClass);
+    this.$el.find(this.buttonIconSelector).toggleClass([this.buttonOpenClass, this.buttonClosedClass].join(' '));
 }
 
 CallToActionBar.prototype.getTemplateContext = function () {
-  var additionalContext = {};
+    var additionalContext = {};
 
-  additionalContext.ctas = this.opts.content.links.cta.slice();
+    additionalContext.ctas = this.opts.content.links.cta.slice();
 
-  var parentContentCardEl = this.el.parentElement || document.querySelector('[data-content-id="' + this.opts.content.id + '"]');
-  var numOfCtas = additionalContext.ctas.length;
+    var parentContentCardEl = this.el.parentElement || document.querySelector('[data-content-id="' + this.opts.content.id + '"]');
+    var numOfCtas = additionalContext.ctas.length;
 
-  if (parentContentCardEl) {
-    var totalWidth = parentContentCardEl.offsetWidth;
-    var usableWidth = totalWidth - TOTAL_BAR_MARGIN;
-    var usableSlots = Math.floor(usableWidth / EFFECTIVE_CTA_WIDTH);
+    if (parentContentCardEl) {
+        var totalWidth = parentContentCardEl.offsetWidth;
+        var usableWidth = totalWidth - TOTAL_BAR_MARGIN;
+        var usableSlots = Math.floor(usableWidth / EFFECTIVE_CTA_WIDTH);
 
-    additionalContext.additionalCTAs = false;
+        additionalContext.additionalCTAs = false;
 
-    var usedSlots = usableSlots >= numOfCtas ? numOfCtas : usableSlots;
-    var unusedSlots = usableSlots - usedSlots;
+        var usedSlots = usableSlots >= numOfCtas ? numOfCtas : usableSlots;
+        var unusedSlots = usableSlots - usedSlots;
 
-    if (numOfCtas > usableSlots) {
-      var extraSpaceAfterCtasAdded = usableWidth - ((usedSlots * EFFECTIVE_CTA_WIDTH));
-      if (extraSpaceAfterCtasAdded < ADDITIONAL_CTA_WIDTH) {
-        usedSlots -= 1;
-      }
-      additionalContext.additionalCTAs = true;
-      additionalContext.width = (usedSlots * EFFECTIVE_CTA_WIDTH) + ADDITIONAL_CTA_WIDTH;
-    } else {
-      additionalContext.width = (usedSlots * EFFECTIVE_CTA_WIDTH) - SPACE_BETWEEN_CTAS;
+        if (numOfCtas > usableSlots) {
+            var extraSpaceAfterCtasAdded = usableWidth - ((usedSlots * EFFECTIVE_CTA_WIDTH));
+            if (extraSpaceAfterCtasAdded < ADDITIONAL_CTA_WIDTH) {
+                usedSlots -= 1;
+            }
+            additionalContext.additionalCTAs = true;
+            additionalContext.width = (usedSlots * EFFECTIVE_CTA_WIDTH) + ADDITIONAL_CTA_WIDTH;
+        } else {
+            additionalContext.width = (usedSlots * EFFECTIVE_CTA_WIDTH) - SPACE_BETWEEN_CTAS;
+        }
+
+        additionalContext.mainCTAs = additionalContext.ctas.splice(0, usedSlots);
     }
 
-    additionalContext.mainCTAs = additionalContext.ctas.splice(0, usedSlots);
-  }
-
-  return additionalContext;
+    return additionalContext;
 };
 
 CallToActionBar.prototype.destroy = function () {
-  View.prototype.destroy.call(this);
-  window.removeEventListener('resize', this.render.bind(this));
+    View.prototype.destroy.call(this);
+    window.removeEventListener('resize', this.render.bind(this));
 };
 
 module.exports = CallToActionBar;
